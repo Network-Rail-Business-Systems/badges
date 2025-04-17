83fc76c76284b1a9fcd2c006649887db195f99c4
Anthony Edmonds <anthony.edmonds@live.co.uk>
2025-04-17 10:06:46 +0100

    Update Generator.php (#3)
    * Update Generator.php
    
    * Update Generator.php

--- Generator.php
+++ Generator.php
@@ -206,7 +206,7 @@ class Generator
 
     public function makeViewsBadge(): void
     {
-        $path = $this->getPath('.phpunit.cache/view-render-results.json');
+        $path = $this->getPath('.phpunit.cache/views-results.json');
 
         if (file_exists($path) === false) {
             return;
